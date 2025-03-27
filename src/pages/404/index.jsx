8f62d43b661e18118d21bc9bb370ef67960163ca
Riski Agung <riski97@gmail.com>
2025-03-27 22:38:43 +0700

    add profile page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -3,10 +3,7 @@ const NotFound = () => {
     <div className="h-full w-full flex flex-col items-center justify-center overflow-hidden">
       <h1 className="text-9xl font-bold text-gray-900">404</h1>
       <p className="text-2xl text-gray-600 mt-4">Page Not Found</p>
-      <a
-        href="/"
-        className="mt-8 px-6 py-3 rounded-lg bg-primary-600 text-white hover:bg-primary-500 transition-colors"
-      >
+      <a href="/" className="mt-8 px-6 py-3 rounded-lg btn-primary">
         Back to Home
       </a>
     </div>
--- src/routes/DashboardRoutes.jsx
+++ src/routes/DashboardRoutes.jsx
@@ -5,11 +5,13 @@ import NotFound from "@pages/404";
 import { PageRoute } from "@components/PageRoute.";
 
 const Home = lazy(() => import("@pages/dashboard/Home"));
+const Profile = lazy(() => import("@pages/dashboard/Profile"));
 
 function DashboardRoutes() {
   return (
     <Route path="/" component={DashboardLayout}>
       <PageRoute path="/" title="Home" component={Home} />
+      <PageRoute path="/profile" title="Profile" component={Profile} />
       {/* Not found route */}
       <PageRoute
         path="*paramName"
