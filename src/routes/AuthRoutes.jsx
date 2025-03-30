641cbde5624c4e53676d555d5d1fb56a5f3184bf
Riski Agung <riski97@gmail.com>
2025-03-30 12:10:12 +0700

    update laoding screen

--- src/routes/AuthRoutes.jsx
+++ src/routes/AuthRoutes.jsx
@@ -2,6 +2,7 @@ import { Route } from "@solidjs/router";
 import { lazy } from "solid-js";
 import { AuthLayout } from "@layouts";
 import { PageRoute } from "@components/PageRoute.";
+import Suspense from "@components/Suspense";
 
 const Login = lazy(() => import("@pages/auth/Login"));
 const Register = lazy(() => import("@pages/auth/Register"));
@@ -11,13 +12,21 @@ const ResetPassword = lazy(() => import("@pages/auth/ResetPassword"));
 function AuthRoutes() {
   return (
     <Route path="/auth" component={AuthLayout}>
-      <PageRoute path="/login" title="Login" component={Login} />
-      <PageRoute path="/register" title="Register" component={Register} />
-      <PageRoute path="/otp" title="OTP confirmation" component={Otp} />
+      <PageRoute path="/login" title="Login" component={Suspense(Login)} />
+      <PageRoute
+        path="/register"
+        title="Register"
+        component={Suspense(Register)}
+      />
+      <PageRoute
+        path="/otp"
+        title="OTP confirmation"
+        component={Suspense(Otp)}
+      />
       <PageRoute
         path="/reset-password"
         title="Reset Password"
-        component={ResetPassword}
+        component={Suspense(ResetPassword)}
       />
     </Route>
   );
--- src/routes/DashboardRoutes.jsx
+++ src/routes/DashboardRoutes.jsx
@@ -3,6 +3,7 @@ import { lazy } from "solid-js";
 import { DashboardLayout } from "@layouts";
 import NotFound from "@pages/404";
 import { PageRoute } from "@components/PageRoute.";
+import Suspense from "@components/Suspense";
 
 const Home = lazy(() => import("@pages/dashboard/Home"));
 const Profile = lazy(() => import("@pages/dashboard/Profile"));
@@ -11,14 +12,22 @@ const Products = lazy(() => import("@pages/dashboard/Products"));
 function DashboardRoutes() {
   return (
     <Route path="/" component={DashboardLayout}>
-      <PageRoute path="/" title="Home" component={Home} />
-      <PageRoute path="/profile" title="Profile" component={Profile} />
-      <PageRoute path="/products" title="Products" component={Products} />
+      <PageRoute path="/" title="Home" component={Suspense(Home)} />
+      <PageRoute
+        path="/profile"
+        title="Profile"
+        component={Suspense(Profile)}
+      />
+      <PageRoute
+        path="/products"
+        title="Products"
+        component={Suspense(Products)}
+      />
       {/* Not found route */}
       <PageRoute
         path="*paramName"
         title="Page not found"
-        component={NotFound}
+        component={Suspense(NotFound)}
       />
     </Route>
   );
